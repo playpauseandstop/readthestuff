69cc82c097d1b49976e333f120261d9144c4a28a
Igor Davydenko <iam@igordavydenko.com>
2013-09-29 00:24:50 +0300

    Subscriptions schema update.

--- readthestuff/sql/initial.sql
+++ readthestuff/sql/initial.sql
@@ -1,7 +1,7 @@
 -- Initail database schema
 
 CREATE SEQUENCE subscription_id_seq;
-CREATE TABLE IF NOT EXISTS subcsriptions (
+CREATE TABLE IF NOT EXISTS subscriptions (
     id integer PRIMARY KEY DEFAULT nextval('subscription_id_seq'),
     href varchar(255) NOT NULL UNIQUE,
 
@@ -9,9 +9,18 @@ CREATE TABLE IF NOT EXISTS subcsriptions (
     received_at timestamp with time zone
 );
 
+CREATE SEQUENCE tag_id_seq;
+CREATE TABLE IF NOT EXISTS tags (
+    id integer PRIMARY KEY DEFAULT nextval('tag_id_seq'),
+    name varchar(255) NOT NULL UNIQUE
+);
+
+CREATE SEQUENCE user_subscription_id_seq;
 CREATE TABLE IF NOT EXISTS user_subscriptions (
+    id integer PRIMARY KEY DEFAULT nextval('user_subscription_id_seq'),
+
     user_id integer NOT NULL,
-    subscription_id integer NOT NULL,
+    subscription_id integer NOT NULL REFERENCES subscriptions(id) ON DELETE CASCADE,
 
     title varchar(64) NOT NULL,
     link varchar(255) NOT NULL DEFAULT '',
@@ -19,6 +28,17 @@ CREATE TABLE IF NOT EXISTS user_subscriptions (
 
     unread_counter integer NOT NULL DEFAULT 0,
 
-    CONSTRAINT unique_ids_pair UNIQUE (user_id, subscription_id),
-    CONSTRAINT unsigned_unread_counter CHECK (unread_counter > 0)
+    CONSTRAINT unique_ids_us UNIQUE (user_id, subscription_id),
+    CONSTRAINT unsigned_unread_counter_us CHECK (unread_counter >= 0)
+);
+
+CREATE TABLE IF NOT EXISTS user_tag_subscriptions (
+    user_id integer NOT NULL,
+    tag_id integer NOT NULL REFERENCES tags(id) ON DELETE CASCADE,
+    user_subscription_id integer NOT NULL REFERENCES user_subscriptions(id) ON DELETE CASCADE,
+
+    unread_counter integer NOT NULL DEFAULT 0,
+
+    CONSTRAINT unique_ids_uts UNIQUE (user_id, tag_id, user_subscription_id),
+    CONSTRAINT unsigned_unread_counter_uts CHECK (unread_counter >= 0)
 );
